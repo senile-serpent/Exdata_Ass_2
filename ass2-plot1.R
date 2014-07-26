d116f8427b0b3f9785a59f35434f1825ab866a30
unknown <10019319@Vorstera.gijima.com>
2014-07-26 13:45:00 +0200

    Clunky code for Plot 1

---
+++ ass2-plot1.R
@@ -0,0 +1,18 @@
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+sum99 <- sum(nei[nei$year == "1999",4])
+sum02 <- sum(nei[nei$year == "2002",4])
+sum05 <- sum(nei[nei$year == "2005",4])
+sum08 <- sum(nei[nei$year == "2008",4])
+
+yrs <- as.numeric(c( "1999", "2002", "2005","2008"))
+
+vals <- c(sum99, sum02, sum05, sum08)
+
+png("plot1.png",width=480,height=480,units='px')
+
+plot(yrs, vals, type = "o", main = "Total PM25 emissions from all sources", xlab = "Years", ylab = "Total")
+
+dev.off()
+
